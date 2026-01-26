ec3159fdb66323cebf6ca3aa2904a76789103b29
Alex <yevsyukovaleks@gmail.com>
2026-01-27 01:30:43 +0300

    Add models

---
+++ user-service/src/main/kotlin/ru/hse/hasslspace/userservice/model/Verification.kt
@@ -0,0 +1,34 @@
+package ru.hse.hasslspace.userservice.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+import ru.hse.hasslspace.userservice.model.Verification.Companion.TABLE_NAME
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Table(name = TABLE_NAME)
+data class Verification(
+    @Id
+    @Column(ID_COLUMN_NAME)
+    val id: UUID? = null,
+
+    @Column(USER_ID_COLUMN_NAME)
+    val userId: String,
+
+    @Column(CODE_COLUMN_NAME)
+    val code: String,
+
+    @Column(CREATED_AT_COLUMN_NAME)
+    val createdAt: LocalDateTime?
+) {
+
+    companion object {
+        const val TABLE_NAME = "verification"
+
+        const val ID_COLUMN_NAME = "id"
+        const val USER_ID_COLUMN_NAME = "user_id"
+        const val CODE_COLUMN_NAME = "code"
+        const val CREATED_AT_COLUMN_NAME = "created_at"
+    }
+}
